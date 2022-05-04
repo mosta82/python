4471ca4d3060a53f22ed24fb2029fc7aa2affed5
root <root@localhost>
2022-05-05 00:02:13 +0600

    auto publish 1651687333805

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
-def brush_teeth():
-  print("take the toothbrush")
-  print("put paste on the brush")
-  print("clean teeth")
-  print("wash the toothbrush")
-  print("rinse mouth")
-brush_teeth()
+def add_numbers(first, second):
+   sum = first + second
+   print(sum)
+
+add_numbers(13, 53)

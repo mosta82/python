b26e7ba720f7cb8c5273eb1dbcb62d9c75d50748
root <root@localhost>
2022-05-05 00:50:23 +0600

    auto publish 1651690223227

--- src/main.py
+++ src/main.py
@@ -1,7 +1,18 @@
-def add (a,b):
-    return a + b
+def is_odd(num):
+    if num % 2 == 0:
+        return False
+    else:
+        return True
 
-x = add(2,3)
-y = add(4,5)
-z = add(x, y)
-print(z)
+
+def evenify(num):
+    check_odd = is_odd(num)
+    if check_odd == True:
+        even_num = num * 2
+    else:
+        even_num = num
+    return even_num
+
+    
+result = evenify( 3)
+print(result)

164399d0205f6e3d86e4423545287c7bdc5c6a8b
root <root@localhost>
2022-05-04 23:31:07 +0600

    auto publish 1651685467537

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
-basket = ["Apple", "Orange", "Strawberry", "Banana"]
-for fruit in basket:
-   print(fruit)
+def brush_teeth():
+  print("take the toothbrush")
+  print("put paste on the brush")
+  print("clean teeth")
+  print("wash the toothbrush")
+  print("rinse mouth")
+brush_teeth()

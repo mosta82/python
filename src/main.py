de5d8916a7c03b1f63ea52136eaed5b998a300c0
root <root@localhost>
2022-05-18 08:42:50 +0600

    auto publish 1652841770374

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
-def add_numbers(first, second):
-    sum = first + second
-    print(sum)
-     
-add_numbers(13, 54)
+int_text = input('Give me an integer number: ')
+int_num = int(int_text)
+
+float_text = input('Give me a float  number: ')
+float_num = float(float_text)
+
+result = int_num * float_num
+
+print('Your result is: ', result)

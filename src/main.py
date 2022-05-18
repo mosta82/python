4a355709a563362d46756b43dd01f8335e6775f4
root <root@localhost>
2022-05-18 08:44:12 +0600

    auto publish 1652841852805

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
-int_text = input('Give me an integer number: ')
-int_num = int(int_text)
+int_num = int(input('Give me an integer number: '))
 
-float_text = input('Give me a float  number: ')
-float_num = float(float_text)
+float_num = float(input('Give me a float  number: '))
 
 result = int_num * float_num
 

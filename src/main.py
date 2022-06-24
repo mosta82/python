69085ef0ae0d7c822ddbc52d71af5c94784762aa
root <root@localhost>
2022-06-24 15:47:37 +0600

    auto publish 1656064057152

--- src/main.py
+++ src/main.py
@@ -2,6 +2,6 @@ base_num = int(input('Give me the base number: '))
 
 power_num = int(input('Give me the power number: '))
 
-result = base_num ** power_num
+result = pow(base_num, power_num)
 
 print('Your result is: ', result)

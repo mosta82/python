9400c839fee7b3bb4ded23b8246b2e9cf0fe3d36
root <root@localhost>
2022-07-02 16:42:53 +0600

    auto publish 1656758573647

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
-num1 = int(input('Enter the first number: '))
-num2 = int(input('Enter the second number: '))
- 
-result = num1//num2
-print(result)
+num1 = int(input("First number: "))
+num2 = int(input("Second number: "))
+if (num2 >= num1):
+    largest = num2
+else:
+    largest = num1
+print("Largest number you entered is: ", largest)

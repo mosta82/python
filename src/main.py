03afde7a87efbac5e98f7364078b64568c0ab9af
root <root@localhost>
2022-07-08 15:03:47 +0600

    auto publish 1657271027187

--- src/main.py
+++ src/main.py
@@ -1,11 +1,7 @@
-num = int(input('Insert the number: '))
- 
-result = [] 
-i = 1
- 
-while i<=num:
-   if i%3==0 and i%5==0:
-       result.append(i)
-   i=i+1
- 
-print(result)
+user_numb = int(input("Enter your number: "))
+
+while user_numb > 0:
+    if user_numb % 5 == 0 or user_numb % 3 == 0:
+        print(user_numb)
+
+    user_numb -= 1

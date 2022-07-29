9733d679a74fd9771a4f07671c9fea944e26122f
root <root@localhost>
2022-07-29 17:58:31 +0600

    auto publish 1659095910830

--- src/main.py
+++ src/main.py
@@ -1,5 +1,16 @@
-celsius = float(input("Enter temperature in degrees Celsius: "))
+def dec_to_binary(n):
+    bits = []
+    
+    while n > 0:
+        bits.append(n%2)
+        n = n // 2
+    bits.reverse()
+    
+    binary = ''
+    for bit in bits:
+        binary += str(bit)
+    return binary
  
-fahrenheit = celsius*9/5+32
- 
-print("Temperature in Fahrenheit:", fahrenheit)
+num = int(input("Your decimal number: "))
+binary = dec_to_binary(num)
+print("Your binary is:", binary)
